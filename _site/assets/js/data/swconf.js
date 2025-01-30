ed46e58c24fe4f17dc240e9849d7e6e8590d8b23
Guk Hyun <moonstalker9010@gmail.com>
2025-01-31 01:32:47 +0900

    fix: kakao share update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1738253196',resources: [
+    cacheName: 'chirpy-1738254657',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
