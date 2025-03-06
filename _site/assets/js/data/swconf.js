3e736eb55f9b34a5fdb2ff32b268913865b1e8a9
Guk Hyun <moonstalker9010@gmail.com>
2025-03-06 22:54:03 +0900

    카카오 공유 수정

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741267847',resources: [
+    cacheName: 'chirpy-1741269231',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
