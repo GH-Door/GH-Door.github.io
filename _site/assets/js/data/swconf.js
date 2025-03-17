7a1be2e7596bf06ef2fc99e397bed370e49ef56e
Guk Hyun <moonstalker9010@gmail.com>
2025-03-17 16:09:42 +0900

    update img name

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742193995',resources: [
+    cacheName: 'chirpy-1742195340',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
