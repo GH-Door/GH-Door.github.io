de04f15b0482f22e47b465e89211be3b0bb5654c
Guk Hyun <moonstalker9010@gmail.com>
2025-03-25 12:11:57 +0900

    post update - 빅분기

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1742570463',resources: [
+    cacheName: 'chirpy-1742872289',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
