b80436079b163e82cbf4db361e6180aa69b4e5b4
Guk Hyun <moonstalker9010@gmail.com>
2025-04-01 20:33:27 +0900

    big-data post update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1743331563',resources: [
+    cacheName: 'chirpy-1743507147',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
